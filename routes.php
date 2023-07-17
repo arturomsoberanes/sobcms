c2aac686241d30ebd3c14007c6504da9403cdfef
Arturo Soberanes <arturo.soberanes01@gmail.com>
2023-07-16 20:11:05 -0600

    The routes for show form and add users moved to the protected group

--- routes.php
+++ routes.php
@@ -15,12 +15,12 @@ SimpleRouter::get('/not-found', [HomeController::class, 'showNotFound']);
 SimpleRouter::get('/login', [LoginController::class, 'showLogin']);
 SimpleRouter::get('/logout', [LoginController::class, 'logout']);
 SimpleRouter::post('/login', [LoginController::class, 'login']);
-SimpleRouter::get('/signin', [LoginController::class, 'showSignin']);
-SimpleRouter::post('/signin', [UserController::class, 'addUser']);
 
 SimpleRouter::group(['middleware' => AdminMiddleware::class], function () {
   SimpleRouter::get('/admin', [AdminController::class, 'showAdmin']);
   SimpleRouter::get('/admin/write/{id?}', [AdminController::class, 'showWritePost']);
+  SimpleRouter::get('/signin', [LoginController::class, 'showSignin']);
+  SimpleRouter::post('/signin', [UserController::class, 'addUser']);
 });
 
 
