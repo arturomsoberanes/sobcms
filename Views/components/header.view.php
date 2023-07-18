398b67eb880b1148da2867ef033a87e0c7cdc4d6
Arturo Soberanes <arturo.soberanes01@gmail.com>
2023-07-18 15:22:06 -0600

    Added Class text-center to header

--- Views/components/header.view.php
+++ Views/components/header.view.php
@@ -1,6 +1,6 @@
 <header class="w-100 text-white">
   <div class="header full-screen">
-    <h1><?= $title?></h1>
+    <h1 class="text-center"><?= $title?></h1>
     <?php require 'Views/components/navbar.view.php';?>
   </div>
 </header>
