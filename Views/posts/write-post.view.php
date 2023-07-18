9ad2941380d05f4c811e1e3afe2fcda49e4d0631
Arturo Soberanes <arturo.soberanes01@gmail.com>
2023-07-18 11:52:59 -0600

    Fix route when the form is sent and input for images added

--- Views/posts/write-post.view.php
+++ Views/posts/write-post.view.php
@@ -1,21 +1,29 @@
 <section class="section-write">
   <article>
-    <form action="<?=$post ? 'admin/write/' . $post->id : 'admin/new-post'?>" method="post">
+    <form action="<?= $post ? 'write/' . $post->id : 'new-post' ?>" method="post" enctype="multipart/form-data">
       <div class="mb-3">
         <label for="title" class="form-label">Title:</label>
-        <input type="text" class="form-control" id="title" name="title" aria-describedby="title" value="<?= $post ? $post->title : '';?>" />
+        <input type="text" class="form-control" id="title" name="title" aria-describedby="title"
+          value="<?= $post ? $post->title : ''; ?>" />
       </div>
       <div class="mb-3">
         <label for="excerpt" class="form-label">Excerpt:</label>
-        <input type="text" class="form-control" id="excerpt" name="excerpt" aria-describedby="excerpt" value="<?= $post ? $post->excerpt : '';?>" />
+        <input type="text" class="form-control" id="excerpt" name="excerpt" aria-describedby="excerpt"
+          value="<?= $post ? $post->excerpt : ''; ?>" />
       </div>
       <div class="mb-3">
         <label for="content" class="form-label">Content:</label>
         <textarea name="content" id="content" cols="30" rows="10">
-          <?= $post ? $post->content : '';?>
+          <?= $post ? $post->content : ''; ?>
         </textarea>
       </div>
-      <button type="submit" class="btn btn-warning"><?=$post ? 'Update' : 'Add'?></button>
+      <div class="mb-3">
+        <label for="featured_image" class="form-label">Featured Image (Optional):</label>
+        <input class="form-control" type="file" id="featured_image" name="featured_image">
+      </div>
+      <button type="submit" class="btn btn-warning">
+        <?= $post ? 'Update' : 'Add' ?>
+      </button>
     </form>
   </article>
 </section>
