84d3499a88345cc93a88861fc9550889a9d1bba0
Arturo Soberanes <arturo.soberanes01@gmail.com>
2023-07-20 17:24:18 -0600

    Featured image added to list of posts and new styles for featured image and list of posts

--- Views/posts/list-posts.view.php
+++ Views/posts/list-posts.view.php
@@ -2,20 +2,29 @@
 <section class="text-white w-100 section-blog">
   <?php foreach ($posts as $post): ?>
     <article class="card text-bg-dark mb-3">
-      <div class="card-body">
-        <h5 class="card-title">
-          <?= $post->title; ?>
-          </h5>
-          <p class="card-text">
-            <?= $post->excerpt; ?>
-          </p>
-          <blockquote class="blockquote mb-0">
-            <footer class="blockquote-footer">Published On: <?=showDate($post->published_on)?></footer>
-          </blockquote>
-          <a href="posts/<?= $post->id ?>" class="my-2 btn btn-warning">
-            Show more...
-          </a>
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="<?= $post->featured_image; ?>" class="img-fluid rounded-start" alt="featured image">
         </div>
-      </article>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">
+              <?= $post->title; ?>
+            </h5>
+            <p class="card-text">
+              <?= $post->excerpt; ?>
+            </p>
+            <blockquote class="blockquote mb-0">
+              <footer class="blockquote-footer">Published On:
+                <?= showDate($post->published_on) ?>
+              </footer>
+            </blockquote>
+            <a href="posts/<?= $post->id ?>" class="my-2 btn btn-warning">
+              Show more...
+            </a>
+          </div>
+        </div>
+      </div>
+    </article>
   <?php endforeach; ?>
 </section>
