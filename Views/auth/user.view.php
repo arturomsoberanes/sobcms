08695e5ea43ddb8457b9624571773df944e7dbe2
Arturo Soberanes <arturo.soberanes01@gmail.com>
2023-07-23 15:24:53 -0600

    Now the user view can be used for update users too, if the form have the information of user this values will be passed to the inputs

--- Views/auth/user.view.php
+++ Views/auth/user.view.php
@@ -3,21 +3,24 @@
     <form action="/admin/user/<?= $user ? "update/{$user->id}" : 'add' ?>" method="post">
       <div class="mb-3">
         <label for="name" class="form-label">Name:</label>
-        <input type="text" class="form-control" id="name" name="name" aria-describedby="name" required>
+        <input type="text" class="form-control" id="name" name="name" aria-describedby="name" value="<?= $user ? $user->name : ''?>" <?= !$user && 'required'?> />
       </div>
       <div class="mb-3">
         <label for="username" class="form-label">Username:</label>
-        <input type="text" class="form-control" id="username" name="username" aria-describedby="username" required>
+        <input type="text" class="form-control" id="username" name="username" aria-describedby="username" value="<?= $user ? $user->username : ''?>" <?= !$user && 'required'?> />
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email address:</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="email" required>
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="email" value="<?= $user ? $user->email : ''?>" <?= !$user && 'required'?> />
       </div>
       <div class="mb-3">
         <label for="password" class="form-label">Password:</label>
-        <input type="password" class="form-control" id="password" name="password" required/>
+        <input type="password" class="form-control" id="password" name="password" <?= !$user && 'required'?> />
       </div>
-      <button type="submit" class="btn btn-warning">Add User</button>
+      <button type="submit" class="btn btn-warning">
+        <?= $user ? 'Update User' : 'Add User' ?>
+      </button>
+      <a href="/admin" class="btn btn-secondary">Cancel</a>
     </form>
   </article>
 </section>
